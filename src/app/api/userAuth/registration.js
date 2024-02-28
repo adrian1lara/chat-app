f14340f4af04a1ee7f71324c8b81cacf96c14339
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-02-28 16:14:58 -0500

    add handle fetch to register user post request

---
+++ src/app/api/userAuth/registration.js
@@ -0,0 +1,31 @@
+
+
+export default async function userRegistration (username, email, password ) {
+    try {
+        const res = await fetch("http://localhost:3000/api/v0/user/new", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify( {
+                username: username,
+                email: email,
+                password: password
+            })
+         })
+    
+         if(res.ok) {
+            // everything is ok
+            console.log("Registration successfully")
+            return {success: true}
+         } else {
+             const dataError = res.text()
+             return dataError
+         }
+
+    } catch (error) {
+        console.error(error)
+    }
+    
+
+}
