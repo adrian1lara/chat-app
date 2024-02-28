08b42d25d52ec85ad7cfdc16a2fca01e038f9a70
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-02-27 22:22:09 -0500

    add login page and form component

---
+++ src/app/log-in/page.js
@@ -0,0 +1,11 @@
+import LogInForm from "../components/loginForm";
+
+
+export default function LogInPage() {
+    return (
+        <div>
+            <h1>Log In</h1>
+            <LogInForm />
+        </div>
+    )
+}
