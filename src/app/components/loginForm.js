881f16dd5af6fa702cdd134f1a50bd5e72f0e2ad
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-02-27 22:22:45 -0500

    add structure of login form

---
+++ src/app/components/loginForm.js
@@ -0,0 +1,10 @@
+ import "../styles/global.css"
+ 
+ export default function LogInForm () {
+    return (
+        <div>
+            <input type="email" name="email" placeholder="example@domain.com"></input>
+            <input type="password" name="password" placeholder="*******"></input>
+        </div>
+    )
+ }
