49e5d37ed9cc55c89a6d65724ef6b762266fcf4e
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-02-28 21:43:57 -0500

    add header and navigation component

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,16 @@
+import "./styles/global.css"
 import Image from "next/image";
+import NoUserNav from "./components/noUserNav";
 
 export default function Home() {
   return (
-   <main>
-    <h1>Hello World</h1>
-   </main>
+    <div className="w-screen border-2 border-black">
+      <header>
+          <NoUserNav />
+      </header>
+      <main>
+      <h1>Hello World</h1>
+      </main>
+    </div>
   );
 }
