5bc8d948eff67d4f1662a97488bf6591e16fc129
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-02-29 12:09:16 -0500

    add usernav component in chatPage

---
+++ src/app/(user)/chat/page.js
@@ -0,0 +1,11 @@
+import "../../styles/global.css"
+import UserNav from "@/app/components/userNav";
+
+
+export default function ChatPage() {
+    return (
+        <div>
+            <UserNav />
+        </div>
+    )
+}
