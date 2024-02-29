52c054076852d4cfc99b37d15180c7457a1e473b
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-02-29 00:08:51 -0500

    nav for user not logged

--- src/app/components/noUserNav.js
+++ src/app/components/noUserNav.js
@@ -8,16 +8,18 @@ export default function NoUserNav() {
                 className="font-bold text-2xl tracking-wide"
                 >IChat</h1>
                 <div className="flex">
-                    <button
+                    <a
                     className="middle none center mr-3 rounded-lg bg-pink-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                    href="/log-in"
                     >
                         Log-in
-                    </button>
-                    <button 
+                    </a>
+                    <a 
                     className="middle none center mr-3 rounded-lg bg-pink-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                    href="/sign-up"
                     >
                         Sign-up
-                    </button>
+                    </a>
                 </div>
             </nav>
         </div>
