789b0d7d44c31558daaac0458681829a964d3108
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-02-28 21:43:20 -0500

    navigation bar for no logged users

---
+++ src/app/components/noUserNav.js
@@ -0,0 +1,25 @@
+
+
+export default function NoUserNav() {
+    return (
+        <div>
+            <nav className="w-full flex justify-between m-1">
+                <h1 
+                className="font-bold text-2xl tracking-wide"
+                >IChat</h1>
+                <div className="flex">
+                    <button
+                    className="middle none center mr-3 rounded-lg bg-pink-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                    >
+                        Log-in
+                    </button>
+                    <button 
+                    className="middle none center mr-3 rounded-lg bg-pink-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                    >
+                        Sign-up
+                    </button>
+                </div>
+            </nav>
+        </div>
+    )
+}
