81dd54b7eadb0ac4c7a0d347362d128a446b1ec2
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-03-01 00:12:07 -0500

    update name class to className

--- src/app/sign-up/page.js
+++ src/app/sign-up/page.js
@@ -5,8 +5,8 @@ import SignUpForm from "../components/signUpForm";
 export default function SignUpPage () {
 
     return(
-        <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-            <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-fuchsia-400">Sign Up</h2>
+        <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-fuchsia-400">Sign Up</h2>
             <SignUpForm />
         </div>
     )
