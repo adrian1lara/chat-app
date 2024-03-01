d279d76c6ac0730e69f81de0635bb77f238c6ff6
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-03-01 00:13:50 -0500

    add handle fetch data of current user

--- src/app/(user)/chat/page.js
+++ src/app/(user)/chat/page.js
@@ -1,20 +1,42 @@
 'use client'
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "../../styles/global.css"
 import UserNav from "@/app/components/userNav";
 import { validateUser } from "@/app/middleware/validateUser";
 import { useRouter } from "next/navigation";
+import getCurrentUser from "@/app/api/userAuth/currentUser";
 
 
 export default function ChatPage() {
+    const [data, setData] = useState('')
+
     const router = useRouter()
-    
+
     useEffect(() => {
+
+        const token = localStorage.getItem('accessToken')
+
         validateUser(router)
-    })
+        
+        const  fetchData = async () => {
+            try {
+                const res = await getCurrentUser(token)
+
+                setData(res)
+            } catch (error) {
+                console.error(error)
+            }
+        }
+
+        fetchData()
+
+    }, [])
+
+    if(!data) return <h1>Loading...</h1>
+    
     return (
         <div>
-            <UserNav />
+            <UserNav data={data}/>
         </div>
     )
 }
