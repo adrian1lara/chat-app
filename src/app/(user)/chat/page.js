9b825ae53064a870bad4d07bdd0ab91a08e2321e
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-02-29 21:38:24 -0500

    add mw validauser

--- src/app/(user)/chat/page.js
+++ src/app/(user)/chat/page.js
@@ -1,8 +1,17 @@
+'use client'
+import { useEffect } from "react";
 import "../../styles/global.css"
 import UserNav from "@/app/components/userNav";
+import { validateUser } from "@/app/middleware/validateUser";
+import { useRouter } from "next/navigation";
 
 
 export default function ChatPage() {
+    const router = useRouter()
+    
+    useEffect(() => {
+        validateUser(router)
+    })
     return (
         <div>
             <UserNav />
