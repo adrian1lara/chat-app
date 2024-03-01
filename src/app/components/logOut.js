8c5a9f46b094271eb9399d557be09c3f31aa0064
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-02-29 21:37:28 -0500

    add component button to log out user

---
+++ src/app/components/logOut.js
@@ -0,0 +1,15 @@
+
+
+export default function LogOutBtn () {
+    
+    const clearStorage = () => {
+        localStorage.clear()
+    }
+
+    return (
+        <button class="flex block rounded-md px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 active:bg-blue-100 cursor-pointer" role="menuitem"
+        onClick={() => clearStorage}>
+            Log out           
+        </button>
+    )
+}
