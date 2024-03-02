755d34ac6b1c13dcb2dba1c644f6ab221b318710
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-03-01 23:25:26 -0500

    add component user avatar for profile page

---
+++ src/app/components/profileAvatar.js
@@ -0,0 +1,10 @@
+import avatarDefault from "../assets/defaultavatar.png"
+import Image from "next/image"
+export default function ProfileAvatar ({img}) {
+
+    return (
+        <Image className="max-w-40 max-h-40 rounded-lg" 
+        src={img ? img : avatarDefault} 
+        alt="profile image"/>
+    )
+}
