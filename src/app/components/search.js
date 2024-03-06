26d561b647a7718feee6f87fdc8f1123b5b0de64
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-03-06 00:35:52 -0500

    add div around the input

--- src/app/components/search.js
+++ src/app/components/search.js
@@ -12,10 +12,12 @@ export default function SearchBar ({onSearch}) {
 
 
     return (
-
-        <input className="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none" 
-        type="search" placeholder="Search usernames" value={searchTerm} onChange={handleSearchTerm}>
-        </input>
+        <div className="w-md">
+             <input className="border-2 border-gray-300 bg-white p-2 rounded-lg focus:outline-none" 
+            type="search" placeholder="Search usernames" value={searchTerm} onChange={handleSearchTerm}>
+            </input>
+        </div>
+       
             
     )
 }
