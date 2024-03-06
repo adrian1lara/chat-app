161f8ea445703788cb1da4d1a3b8a85e8ed36c5a
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-03-05 23:14:01 -0500

    fix type error class to className

--- src/app/components/signUpForm.js
+++ src/app/components/signUpForm.js
@@ -40,7 +40,7 @@ export default function SignUpForm () {
                 <input type="password" name="password" placeholder="********"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
-                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 px-2 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></input>
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 px-2 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></input>
                  {error && ( // Conditionally render error message
                     <p className="text-red-500 text-center">{error}</p>
                     )}
