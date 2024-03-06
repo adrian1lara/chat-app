f5ec1dc9cad48c7744fc18b98338fcd74af24c37
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-03-06 00:34:47 -0500

    add reload window event

--- src/app/components/logOut.js
+++ src/app/components/logOut.js
@@ -4,11 +4,12 @@ export default function LogOutBtn () {
     
     const clearStorage = () => {
         localStorage.clear()
+        location.reload()
     }
 
     return (
         <button className="flex block rounded-md px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 active:bg-blue-100 cursor-pointer" role="menuitem"
-        onClick={() => clearStorage}>
+        onClick={clearStorage}>
             Log out           
         </button>
     )
