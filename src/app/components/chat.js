fc1a49be18d867f83a0faf267a6fe8c3cd21aef2
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-03-08 21:22:27 -0500

    add: display messages

--- src/app/components/chat.js
+++ src/app/components/chat.js
@@ -1,19 +1,20 @@
+
 import Message from "./message";
 
-export default function UserChat () {
+export default function UserChat ({ messages, userId }) {
+    
+    //if(!messages) { return "loading..."}
     return (
         <div className="px-5 flex flex-col justify-between border-2 h-full">
             <div className="px-5 py-5 flex justify-between items-center bg-white border-b-2">
-                <h1>Chat User</h1>
+                <h1>chat with <span className="text-blue-700">{userId}</span></h1>
             </div>
             <div className="flex flex-col space-y-4 p-3 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch">
-                <div className="flex justify-end mb-4">
-                    <p>Content of the chat</p>
-                </div>
-                <div className="flex justify-start mb-4">
-                    <p>Content of the chat</p>
-                </div>
-                
+            { messages && messages.map((message) => (
+            <div key={message._id} className={`flex justify-${message.sender === userId ? 'end' : 'start'} mb-4`} >
+                <p>{message.content}</p> {/* Render message content */}
+            </div>
+            ))}
             </div>
             <div>
                 <Message />
