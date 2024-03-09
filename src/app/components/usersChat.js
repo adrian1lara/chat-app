1c0f1999e41dcc842ab9df6b16fb68d7b695e6e7
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-03-08 21:23:13 -0500

    add: on selectuser prop to usernames.map key

--- src/app/components/usersChat.js
+++ src/app/components/usersChat.js
@@ -2,14 +2,17 @@ import { useEffect, useState } from "react";
 import UserAvatar from "./avatar";
 
 
-export default function UserDisplay({usernames}) {
+export default function UserDisplay({usernames, onUserSelect }) {
+
+    
    
     return (
         <div className="overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded ">
             {usernames && (
-                <div className="w-md space-y-5">
+                <div className="w-md  space-y-5">
                     {usernames.map((user) => (
-                        <div key={user._id} className="flex items-center border-2 border-slate p-2 max-w-sm rounded-2xl">
+                        <div key={user._id} className="flex items-center cursor-pointer hover:bg-gray-200 border-2 border-slate p-2 max-w-sm rounded-2xl"
+                        onClick={()=> onUserSelect(user._id)}>
                             <UserAvatar  img={user.avatar}/>
                             <p className="text-xl">{user.username}</p>
                             
