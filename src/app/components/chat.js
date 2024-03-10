6f2fa0a4dc69d617e6c052d4b2ea1e4c561d2a26
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-03-10 00:29:10 -0500

    add handle send new message and display messages

--- src/app/components/chat.js
+++ src/app/components/chat.js
@@ -1,24 +1,36 @@
 
 import Message from "./message";
+import sendMessage from "../api/sendMessage";
+
 
 export default function UserChat ({ messages, userId, token, chatData , sender}) {
+
+    
+    const handleSendMessage = async(message)=> {
+        const chat = chatData[0]
+        const chatId = chat._id
+
+        console.log(chatId)
+        console.log(message)
+        await sendMessage(chatId, token, message)
+    }
     
     //if(!messages) { return "loading..."}
     return (
         <div className="px-5 flex flex-col justify-between border-2 h-full">
-            <div className="px-5 py-5 flex justify-between items-center bg-white border-b-2">
-                <h1>chat with <span className="text-blue-700">{userId}</span></h1>
+      {/* ... chat header */}
+      <h2>chat with {userId}</h2>
+      <div className="flex flex-col space-y-4 p-3 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch">
+        {messages.length > 0 &&
+          messages.map((message, index) => (
+           <div key={index} className={`border-2 border-black text-2xl flex ${message.sender == sender ? 'justify-end' : 'justify-start'}`}>
+            <p>{message.content}</p>
             </div>
-            <div className="flex flex-col space-y-4 p-3 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch">
-            { messages && messages.map((message) => (
-            <div key={message._id} className={`border-2 border-black text-2xl flex ${message.sender == sender ? 'justify-end' : 'justify-start'}`} >
-                <p className="w-fit border-2 border-blue-700">{message.content}</p> {/* Render message content */}
-            </div>
-            ))}
-            </div>
-            <div>
-                <Message chatData={chatData} token={token} />
-            </div>
-        </div>
-    )
+          ))}
+      </div>
+      <div>
+        <Message sendMessage={handleSendMessage} />
+      </div>
+    </div>
+  );
 }
