0f2a8ec3fa6fe1472300cbe4d6c8f947fbff5cf0
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-03-12 18:09:48 -0500

    add chat icon and tw styles

--- src/app/components/userNav.js
+++ src/app/components/userNav.js
@@ -1,3 +1,5 @@
+import { AiOutlineMessage } from "react-icons/ai"
+import { LuMessagesSquare } from "react-icons/lu"
 import Dropdown from "./dropdown";
 
 
@@ -6,14 +8,21 @@ export default function UserNav({data}) {
     return (
         <div className="max-w-screen p-4">
             <nav className="flex justify-between items-center">
-                <h1 className="font-bold text-2xl antialiased tracking-widest uppercase">
-                    <a href="/chat">Chatty</a>
-                    </h1>
-                    <div className="border-2 rounded-lg
-                    hover:bg-slate-200 p-2">
-                        <a href="/chat" className="w-full">Chat</a>
-                    </div>
-
+                <h1 className="font-bold flex items-center text-2xl antialiased tracking-widest uppercase space-x-2">
+                <LuMessagesSquare/>
+                    <a href="/chat">
+                        Chatty
+                    </a>
+                    
+                </h1>
+                <ul>
+                    <li >
+                        <a href="/chat" className="p-2 text-lg  rounded-lg 
+                        transition duration-300 ease-in-out
+                        hover:bg-slate-200">Chat</a>
+                    </li>
+                </ul>
+               
                 <Dropdown data={data} />
             </nav>
         </div>
