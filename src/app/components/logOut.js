2edf0557046040fc1e69131e3caea5ff71908ead
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-03-12 18:09:11 -0500

    add log out icon

--- src/app/components/logOut.js
+++ src/app/components/logOut.js
@@ -1,4 +1,4 @@
-
+import { IoLogInOutline } from "react-icons/io5"
 
 export default function LogOutBtn () {
     
@@ -8,9 +8,10 @@ export default function LogOutBtn () {
     }
 
     return (
-        <button className="flex block rounded-md px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 active:bg-blue-100 cursor-pointer" role="menuitem"
+        <button className="flex block rounded-md px-4 py-2 text-sm text-gray-700 hover:bg-red-500 hover:text-white active:bg-blue-100 cursor-pointer" role="menuitem"
         onClick={clearStorage}>
-            Log out           
+            Log out
+            <IoLogInOutline size={20}/>      
         </button>
     )
 }
