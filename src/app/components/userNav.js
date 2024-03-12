77c557a755c4d2dee49f01504cbee1ea862eca08
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-03-12 17:45:21 -0500

    remove link to home, profile and chat

--- src/app/components/userNav.js
+++ src/app/components/userNav.js
@@ -4,26 +4,15 @@ import Dropdown from "./dropdown";
 export default function UserNav({data}) {
 
     return (
-        <div className="max-w-screen m-4 ">
-            <nav className="flex justify-between">
-                <h1>ChatApp</h1>
-
-                <div>
-                    <ul className="flex space-x-3">
-                        <li >
-                            <a href="/home"
-                            className="px-3 py-2 rounded-lg hover:bg-slate-100 ">Home</a>
-                        </li>
-                        <li> 
-                            <a href="/chat"
-                            className="px-3 py-2 rounded-lg hover:bg-slate-100 ">Chat</a>
-                            </li>
-                        <li>
-                            <a href="/profile"
-                            className="px-3 py-2 rounded-lg hover:bg-slate-100 ">Profile</a>
-                        </li>
-                    </ul>
-                </div>
+        <div className="max-w-screen p-4">
+            <nav className="flex justify-between items-center">
+                <h1 className="font-bold text-2xl antialiased tracking-widest uppercase">
+                    <a href="/chat">Chatty</a>
+                    </h1>
+                    <div className="border-2 rounded-lg
+                    hover:bg-slate-200 p-2">
+                        <a href="/chat" className="w-full">Chat</a>
+                    </div>
 
                 <Dropdown data={data} />
             </nav>
