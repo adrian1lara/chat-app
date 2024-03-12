0528b324af8e15cfa4aae44d51586885ff460601
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-03-12 00:15:25 -0500

    add: handle click to active chat bg color

--- src/app/components/usersChat.js
+++ src/app/components/usersChat.js
@@ -2,20 +2,27 @@ import { useEffect, useState } from "react";
 import UserAvatar from "./avatar";
 
 
-export default function UserDisplay({usernames, onUserSelect }) {
+export default function UserDisplay({usernames, onUserSelect}) {
 
-    
-   
+    const [selectedUser, setSelectedUser] = useState(null);
+
+  const handleClick = (userId) => {
+    setSelectedUser(userId === selectedUser ? null : userId);
+  };
+
+
     return (
         <div className="overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded">
             {usernames && (
-                <div className="w-md  space-y-5">
+                <div className="w-md  space-y-5" style={{maxHeight:"500px"}}>
                     {usernames.map((user) => (
-                        <div key={user._id} className="flex items-center cursor-pointer hover:bg-gray-200 border-2 border-slate p-2 max-w-sm rounded-2xl"
-                        onClick={()=> onUserSelect(user._id)}>
-                            <UserAvatar  img={user.avatar}/>
-                            <p className="text-xl">{user.username}</p>
-                            
+                        <div key={user._id} className={`flex items-center cursor-pointer  hover:bg-gray-200 border-2 border-slate p-2 max-w-sm rounded-2xl
+                        ${selectedUser === user._id ? 'bg-gray-200' : 'bg-none'}`}
+                        onClick={()=>{
+                            handleClick(user._id)
+                             onUserSelect(user._id)}}>
+                            <UserAvatar  img={user.avatar} className/>
+                            <p className="mx-2 text-xl">{user.username}</p>
                             </div>
                     ))}
                     </div>
