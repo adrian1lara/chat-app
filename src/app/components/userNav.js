9215b0613e7a6217a2ff05efda30d8d3abfded9d
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-03-12 21:56:10 -0500

    remove link to chat and add border bottom

--- src/app/components/userNav.js
+++ src/app/components/userNav.js
@@ -6,22 +6,14 @@ import Dropdown from "./dropdown";
 export default function UserNav({data}) {
 
     return (
-        <div className="max-w-screen p-4">
+        <div className="max-w-screen p-4 border-b-2">
             <nav className="flex justify-between items-center">
                 <h1 className="font-bold flex items-center text-2xl antialiased tracking-widest uppercase space-x-2">
                 <LuMessagesSquare/>
                     <a href="/chat">
                         Chatty
                     </a>
-                    
                 </h1>
-                <ul>
-                    <li >
-                        <a href="/chat" className="p-2 text-lg  rounded-lg 
-                        transition duration-300 ease-in-out
-                        hover:bg-slate-200">Chat</a>
-                    </li>
-                </ul>
                
                 <Dropdown data={data} />
             </nav>
