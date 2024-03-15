ec51fa442f361af2dfc33c56b9ae1ea37e521ac1
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-03-14 22:58:31 -0500

    add auth token and return data

--- src/app/api/createChat.js
+++ src/app/api/createChat.js
@@ -1,13 +1,13 @@
 
 
-export default async function createChat(currentUserID, clickedUserID) {
+export default async function createChat(currentUserID, clickedUserID, token) {
 
     try {
         const res = await fetch("http://localhost:3000/api/v0/chat/new", {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
-                //Authorization: `Bearer ${token}`
+                Authorization: `Bearer ${token}`
             },
             body: JSON.stringify({
                 participants: [currentUserID, clickedUserID]
@@ -17,8 +17,8 @@ export default async function createChat(currentUserID, clickedUserID) {
         if(res.ok) {
             const chatData = await res.json();
             console.log("Chat created successfully:", chatData);
+            return chatData
             
-
         } else {
             throw new Error(`Api bad request ${res.status}`)
         }
