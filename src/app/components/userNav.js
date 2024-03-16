9e6d081917cbf1d42a79b6feeb76805e0c5bf963
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-03-15 21:07:51 -0500

    replace logo code for logo component

--- src/app/components/userNav.js
+++ src/app/components/userNav.js
@@ -1,6 +1,7 @@
 import { AiOutlineMessage } from "react-icons/ai"
 import { LuMessagesSquare } from "react-icons/lu"
 import Dropdown from "./dropdown";
+import Logo from "./logoHeader";
 
 
 export default function UserNav({data}) {
@@ -8,13 +9,9 @@ export default function UserNav({data}) {
     return (
         <div className="max-w-screen p-4 border-b-2">
             <nav className="flex justify-between items-center">
-                <h1 className="font-bold flex items-center text-2xl antialiased tracking-widest uppercase space-x-2">
-                <LuMessagesSquare/>
                     <a href="/chat">
-                        Chatty
+                       <Logo fontSize={"text-2xl"}/>
                     </a>
-                </h1>
-               
                 <Dropdown data={data} />
             </nav>
         </div>
