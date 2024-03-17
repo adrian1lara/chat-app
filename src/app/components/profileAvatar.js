6d06bdf210000fb6faddb22474e3f59647b95e50
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-03-17 18:08:17 -0500

    add width and height

--- src/app/components/profileAvatar.js
+++ src/app/components/profileAvatar.js
@@ -1,3 +1,4 @@
+
 import avatarDefault from "../assets/defaultavatar.png"
 import Image from "next/image"
 export default function ProfileAvatar ({img}) {
@@ -5,6 +6,6 @@ export default function ProfileAvatar ({img}) {
     return (
         <Image className="max-w-40 max-h-40 rounded-lg" 
         src={img ? img : avatarDefault} 
-        alt="profile image"/>
+        alt="profile image" width={100} height={100}/>
     )
 }
