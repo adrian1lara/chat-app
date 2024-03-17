0f77a1c599cd40e8a9099c564a4fc5f3d38decb4
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-03-17 18:12:47 -0500

    add array with url of avatar imgs

---
+++ src/app/assets/avatars.js
@@ -0,0 +1,16 @@
+
+export const avatars = [
+    {
+        cat: "https://cdn-icons-png.flaticon.com/512/6872/6872932.png"
+    },
+    {
+        cat: "https://cdn-icons-png.flaticon.com/128/4775/4775537.png"
+    },
+    {
+        cat: "https://cdn-icons-png.flaticon.com/128/1150/1150388.png"
+    },
+    {
+        cat: "https://cdn-icons-png.flaticon.com/128/6454/6454644.png"
+    }
+
+]
