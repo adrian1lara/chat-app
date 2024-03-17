2293a8523ddcc1341af8062d451cbed399f67a0c
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-03-17 18:13:09 -0500

    add alt width and height

--- src/app/components/avatar.js
+++ src/app/components/avatar.js
@@ -5,7 +5,7 @@ export default function UserAvatar({img}) {
     return (
         <Image className="w-10 h-10 rounded-lg" 
         src={img ? img : avatarDefault} 
-        alt=""/>
+        alt="profile img" width={100} height={100} objectFit="scale-down"/>
     )
 
 }
