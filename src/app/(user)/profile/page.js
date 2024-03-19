01cf02e8707783a0420ca3421a8fc3229fe09ffa
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-03-18 20:15:10 -0500

    add footer

--- src/app/(user)/profile/page.js
+++ src/app/(user)/profile/page.js
@@ -1,17 +1,17 @@
 'use client'
-import UserAvatar from '@/app/components/avatar';
+
 import '../../styles/global.css'
 import getCurrentUser from "@/app/api/userAuth/currentUser";
 import UserNav from "@/app/components/userNav";
 import { validateUser } from "@/app/middleware/validateUser";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
-import deleteUserAccount from '@/app/api/userAuth/deleteAccount';
 import ProfileAvatar from '@/app/components/profileAvatar';
 import DeleteButton from '@/app/components/deleteAccButton';
 import Modal from '@/app/components/modal';
 import { avatars } from '@/app/assets/avatars';
 import updateAvatar from '@/app/api/updateAvatar';
+import Footer from '@/app/components/footer';
 
 
 
@@ -52,11 +52,11 @@ export default function Page() {
     if(error) return <h1>Opps an error occurred</h1>
 
     return (
-        <div className='w-screen h-screen border-2 border-black'>
+        <div className='w-screen h-screen'>
             <header>
                 <UserNav data={data}/>
             </header>
-            <div className='flex min-h-full flex-col justify-center px-6 py-12 lg:px-8 border-2'>
+            <div className='flex min-h-full flex-col justify-center px-6 py-12 lg:px-8'>
                     <div className=' flex flex-col items-center justify-center'>   
                     <div className='flex'>
                         <ProfileAvatar img={data.avatar}/> 
@@ -70,6 +70,8 @@ export default function Page() {
                 </div>
                
             </div>
+
+            <Footer />
         </div>
         
        
