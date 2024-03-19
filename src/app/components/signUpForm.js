52a1b0d8989c3ba26b4c6b2825d202436d28391f
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-03-18 19:49:21 -0500

    set maxLenght to 20

--- src/app/components/signUpForm.js
+++ src/app/components/signUpForm.js
@@ -28,6 +28,7 @@ export default function SignUpForm () {
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
             <form className="space-y-6" onSubmit={handleSubmit}>  
                 <input type="text" name="username" placeholder="username"
+                maxLength={20}
                 value={username}
                 onChange={(e) => setUsername(e.target.value)}
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 px-2 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
@@ -40,6 +41,7 @@ export default function SignUpForm () {
                 <input type="password" name="password" placeholder="********"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
+                maxLength={20}
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 px-2 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></input>
                  {error && ( // Conditionally render error message
                     <p className="text-red-500 text-center">{error}</p>
