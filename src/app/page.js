5560dc578390ac30951ee92406bfa819930e076a
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-03-19 21:15:01 -0500

    fix valid token

--- src/app/page.js
+++ src/app/page.js
@@ -3,9 +3,9 @@
 import "./styles/global.css"
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import getCurrentUser from "./api/userAuth/currentUser";
-import { data } from "autoprefixer";
 import tokenValid from "./middleware/tokenValidation";
+import { jwtDecode } from "jwt-decode";
+
 
 export default function Home() {
   const router = useRouter()
@@ -13,16 +13,30 @@ export default function Home() {
 
   useEffect(()=> {
     const token = localStorage.getItem('accessToken')
-    const decodetoken = tokenValid(token)
-
-    if(!token || decodetoken) {
-      router.push("/log-in")
-    } else {
-      router.push("/chat")
+    if(!token) {
+      redirectToLogin()
+      return
     }
 
-  }, [])
+    try {
+      const decodedToken = jwtDecode(token);
+      const valid = tokenValid(decodedToken);
+
+      if (!valid) {
+        redirectToLogin();
+      } else {
+        router.push("/chat");
+      }
+    } catch (error) {
+      console.error('Error processing token:', error);
+      redirectToLogin();
+    }
+
+  }, [router])
   
+  const redirectToLogin = () => {
+    router.push("/log-in");
+  };
 
   return (
     <div className="w-screen border-2 border-black">
