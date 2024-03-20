b51466f806ef5953adb58318783a4e268bb230a6
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-03-19 21:15:35 -0500

    fix expiration time

--- src/app/middleware/tokenValidation.js
+++ src/app/middleware/tokenValidation.js
@@ -1,8 +1,8 @@
-import { jwtDecode } from "jwt-decode";
 
 export default function tokenValid(token) {
-    const decode = jwtDecode(token)
     const now = Date.now() / 1000;
-    const expiry = decode.iat + decode.exp
+
+    const expiry = token.exp
+
     return now < expiry 
 }
