8892f14689a60678339dd3e17694eec7c93631be
Adrian Lara <adrianlara@Adrians-MacBook-Air.local>
2024-03-23 17:42:21 -0500

    add continue as a guest

--- src/app/components/loginForm.js
+++ src/app/components/loginForm.js
@@ -54,6 +54,47 @@ import getApiUrl from "../utility/getApiUrl"
        }
     }
 
+    const handleGuest = async() => {
+        const API_URL = getApiUrl()
+
+        try {
+
+            try {
+     
+                 const res = await fetch(`${API_URL}/api/v0/user/auth/login`,  {
+                     method: 'POST',
+                     headers: {
+                         'Content-Type': 'application/json',
+                     },
+                     body: JSON.stringify({
+                         email: "guest@guest.com",
+                         password: "guest123456"
+                     })
+                 })
+     
+                 if(res.ok) {
+                     const response = await res.text()
+                     localStorage.setItem('accessToken', response)
+                     
+                     const user = await getCurrentUser(response)
+
+                     if(user.role === 'user') {
+                         router.push('/chat')
+                     }
+     
+                 } else {
+                     const dataError = await res.text()
+                     setError(dataError)
+                 }
+     
+            } catch (error) {
+                 console.error(error)
+            }
+        } catch (error) {
+            
+        }
+    }
+
     return (
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
             <form onSubmit={handleLogIn} className="space-y-6">
@@ -68,10 +109,20 @@ import getApiUrl from "../utility/getApiUrl"
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 px-2 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 required></input>
                 {error && (<p className="text-red-500 text-center text-sm">{error}</p>)}
+                <div className="flex flex-col items-center">
                 <button type="submit"
                 className="flex w-full justify-center rounded-md bg-blue-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm ease-in duration-300  hover:bg-blue-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >log in
                 </button>
+                <p>or</p>
+                <button
+                type="button"
+                className="rounded-md bg-blue-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm ease-in duration-300  hover:bg-blue-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                onClick={handleGuest}>
+                    continue as a guest
+                </button>
+                </div>
+               
 
                 <p className="mt-10 text-center text-sm text-gray-500">
                    Don't have an account yet?
